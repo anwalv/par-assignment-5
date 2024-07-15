594291c23ff551a24fb0bd015e2e6846b19557ca
Anna Oleksandrovych <150846802+anwalv@users.noreply.github.com>
2024-07-15 12:05:59 +0300

    +*/- using reverse polish notation

--- par-assignment-5/par-assignment-5.cpp
+++ par-assignment-5/par-assignment-5.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <unordered_map>
-#include <string>
+#include <iostream> 
+#include <iostream> 
+#include <queue> 
+#include <stack> 
+#include <unordered_map> 
+#include <string> 
 
 using namespace std;
 
 bool isOperator(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == ',';
+    return c == '+'  c == '-'  c == '*'  c == '/'  c == '('  c == ')'  c == ',';
 }
 unordered_map<string, int> mathOperators = {
     {"+", 1},
@@ -28,7 +28,7 @@ queue<string> Tokenized(string input) {
             curToken += ch;
         }
         else if (ch == ' ') {
-           
+
             continue;
         }
         else if ((ch == '-' && tokens.size() == 0 && curToken.empty())
@@ -51,15 +51,90 @@ queue<string> Tokenized(string input) {
     return tokens;
 }
 
-int main() {
-    string input = "12 + 34 * ( 56 - 78 )";
-    queue<string> tokens = Tokenized(input);
+queue<string> ReverseNotation(queue<string> tokens) {
+    queue<string> numberQueue;
+    stack<string> operationStack;
+    int length = tokens.size();
+
     while (!tokens.empty()) {
-        cout << tokens.front() << " ";
+        string currentElement = tokens.front();
         tokens.pop();
+
+        if ((currentElement[0] == '-' && currentElement.length() > 1) || isdigit(currentElement[0])) {
+            numberQueue.push(currentElement);
+        }
+        else if (currentElement == "(") {
+            operationStack.push(currentElement);
+        }
+        else if (currentElement == ")") {
+            while (!operationStack.empty() && operationStack.top() != "(") {
+                numberQueue.push(operationStack.top());
+                operationStack.pop();
+            }
+            operationStack.pop();
+        }
+        else {
+            while (!operationStack.empty() && mathOperators[currentElement] <= mathOperators[operationStack.top()]) {
+                numberQueue.push(operationStack.top());
+                operationStack.pop();
+            }
+            operationStack.push(currentElement);
+        }
     }
-    cout << endl;
+
+    while (!operationStack.empty()) {
+        numberQueue.push(operationStack.top());
+        operationStack.pop();
+    }
+
+    return numberQueue;
+}
+
+int resultNumber(queue<string> expression) {
+    stack<int> resultStack;
+
+    while (!expression.empty()) {
+        string currElement = expression.front();
+        expression.pop();
+
+        if ((currElement[0] == '-' && currElement.length() > 1) || isdigit(currElement[0])) {
+            int number = stoi(currElement);
+            resultStack.push(number);
+        }
+        else if (isOperator(currElement[0])) {
+            int operand2 = resultStack.top();
+            resultStack.pop();
+            int operand1 = resultStack.top();
+            resultStack.pop();
+
+            if (currElement == "+") {
+                resultStack.push(operand1 + operand2);
+            }
+            else if (currElement == "-") {
+                resultStack.push(operand1 - operand2);
+            }
+            else if (currElement == "*") {
+                resultStack.push(operand1 * operand2);
+            }
+            else if (currElement == "/") {
+                resultStack.push(operand1 / operand2);
+            }
+        }
+    }
+
+    return resultStack.top();
+}
+
+int main() {
+    string input;
+    cout << " Please? enter expression: " << endl;
+    getline(cin, input);
+    queue<string> tokens = Tokenized(input);
+    queue<string> rpnTokens = ReverseNotation(tokens);
+
+
+    int result = resultNumber(rpnTokens);
+    cout << "Result: " << result << endl;
 
     return 0;
 }
-
